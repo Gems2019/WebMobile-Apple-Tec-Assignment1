6c3b70bb35967f9b289a23c60c925788bdf6878e
hindarmara <hindarmara@gmail.com>
2025-10-04 21:15:02 -0700

    add validation for user birth year

--- ZodiacApp/ZodiacApp/Calculator/CalculatorView.swift
+++ ZodiacApp/ZodiacApp/Calculator/CalculatorView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct CalculatorView: View {
+    @State private var birthYearString: String = "" // holds user input
+    @State private var validationMessage: String = ""
+    @State private var zodiacSign: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section (header: Text("Enter your birth year")){
+                TextField("User birth year", text: $birthYearString)
+                    .keyboardType(.numberPad)
+            }
+            
+            Section {
+                Button("Submit") {
+                    validateInput()
+                }
+            }
+            
+            if !validationMessage.isEmpty {
+                Section {
+                    Text(validationMessage)
+                        .foregroundColor(.red)
+                }
+            }
+            
+            // show DetailZodiacView
+            
+        }
     }
+    
+    private func validateInput() {
+        let trimmedYearString = birthYearString.trimmingCharacters(in: .whitespaces)
+        
+        guard !trimmedYearString.isEmpty else {
+            validationMessage = "Please enter a birth year."
+            return
+        }
+        
+        guard trimmedYearString.count == 4 else {
+            validationMessage = "Please enter a valid 4-digit birth year."
+            return
+        }
+        
+        guard let birthYear = Int(trimmedYearString) else {
+            validationMessage = "Please enter a valid 4-digit birth year."
+            return
+        }
+        
+        guard birthYear >= 1700 && birthYear <= 2025 else {
+            validationMessage = "Please enter a birth year between 1700 and 2025."
+            return
+        }
+        
+        let userZodiacSign = findZodiacSign(forYear: birthYear)
+        zodiacSign = userZodiacSign
+    }
+    
+//    private func findZodiacSign(forYear year: Int) -> String {
+//        if year % 12
+//    }
 }
 
 #Preview {
